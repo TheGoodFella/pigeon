6e143ae3474e1f66d59446e01bcdfbabf9231ef4
SuperBonny <bonora.matteo@gmail.com>
2016-03-20 18:51:43 +0100

    Add autobahn skeleton

--- node/pigeon.js
+++ node/pigeon.js
@@ -1,12 +1,23 @@
-var mysql = require('db-mysql');
+var mysql = require('mysql');
 
-new mysql.Database({
-    hostname: 'localhost'
+var autobahn = require('autobahn');
+var connection = new autobahn.Connection({
+    url: 'ws://localhost:9000'
+    , realm: 'msgs'
+});
+
+var dataBase = mysql.createConnection({
+    host: 'localhost'
     , user: 'pigeon'
     , password: 'pigeon'
-    , database: 'pigeon'
-}).connect(function (err) {
+});
+
+dataBase.connect(function (err) {
     if (err)
         return console.error(err);
-
+    console.log('Connected to DB.');
 });
+
+dataBase.end(function () {
+    Console.log('Disconnected from DB.');
+})
