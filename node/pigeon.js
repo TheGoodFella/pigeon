81d4093ba37fa7157b8c2c8f90de0ce5890028b3
SuperBonny <bonora.matteo@gmail.com>
2016-03-21 21:08:04 +0100

    Fix DB connection

--- node/pigeon.js
+++ node/pigeon.js
@@ -1,16 +1,31 @@
 var mysql = require('mysql');
 
-var autobahn = require('autobahn');
-var connection = new autobahn.Connection({
-    url: 'ws://localhost:9000'
-    , realm: 'msgs'
-});
-
 var dataBase = mysql.createConnection({
     host: 'localhost'
     , user: 'pigeon'
     , password: 'pigeon'
+    , database: 'PIGEON'
 });
+
+var WebSocketServer = require('ws').Server
+    , wss = new WebSocketServer({
+        port: 6969
+    });
+
+wss.on('connection', function connection(ws) {
+    console.log('Connection from: ' + ws._socket.remoteAddress + ':' + ws._socket.remotePort);
+
+    ws.on('message', function (message) {
+        console.log(ws._socket.remoteAddress + ':' + ws._socket.remotePort + ': ' + message);
+
+    });
+
+    ws.on('close', function close() {
+        console.log(ws._socket.remoteAddress + ': ' + ws._socket.remotePort + 'disconnected.');
+    });
+});
+
+
 
 dataBase.connect(function (err) {
     if (err)
@@ -18,6 +33,13 @@ dataBase.connect(function (err) {
     console.log('Connected to DB.');
 });
 
+dataBase.query('SELECT * FROM USERS', function (err, rows) {
+    if (err) throw err;
+
+    console.log('Data received from Db:\n');
+    console.log(rows);
+});
+
 dataBase.end(function () {
-    Console.log('Disconnected from DB.');
+    console.log('Disconnected from DB.');
 })
